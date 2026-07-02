d051d016a245b3c047d79f3135fcf85809a27d01
Pathsharasakon <pathsharasakon@gmail.com>
2026-07-02 13:44:46 +0700

    add array and object

---
+++ my-array.js
@@ -0,0 +1,2 @@
+let food =["pizza", "malatang", "sushi", "burger", "noodles", "salad"];
+console.log(food);
---
+++ my-object.js
@@ -0,0 +1,51 @@
+// "pizza", "malatang", "sushi", "burger", "noodles", "salad"
+let foodDatabase = [
+    {  id:1,
+        name:"pizza", 
+        cuisine:"Italian",
+        origin:"Italy",
+        vegetarian:true,
+        price: 12.99,
+        image: "images/pizza.jpg"
+    },
+    { id:2, 
+        name:"malatang", 
+        cuisine:"Chinese", 
+        origin:"China", 
+        vegetarian:false,
+        price: 10.50,
+        image: "images/malatang.jpg"
+    },
+    { id:3, 
+        name:"sushi", 
+        cuisine:"Japanese", 
+        origin:"Japan", 
+        vegetarian:true,
+        price: 15.00,
+        image: "images/sushi.jpg"
+    },
+    { id:4, 
+        name:"burger", 
+        cuisine:"American", 
+        origin:"USA", 
+        vegetarian:false,
+        price: 11.99,
+        image: "images/burger.jpg"
+    },
+    { id:5, 
+        name:"noodles", 
+        cuisine:"Chinese", 
+        origin:"China", 
+        vegetarian:false,
+        price: 9.50,
+        image: "images/noodles.jpg"
+    },
+    { id:6, 
+        name:"salad", 
+        cuisine:"Vegetarian", 
+        origin:"Greece", 
+        vegetarian:true,
+        price: 8.99,
+        image: "images/salad.jpg"
+    }
+];
